a388decb59ab5f04adce5af2b39cf1b9de19c0f1
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2023-09-06 19:47:03 -0300

    initial commit to react

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import { FooterContainer } from './styles'
+
+export function Footer() {
+  return (
+    <FooterContainer>
+      <span>Em desenvolvimento por Bernardo Costa Nascimento.</span>
+    </FooterContainer>
+  )
+}
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const FooterContainer = styled.footer`
+  margin-top: auto;
+
+  color: #e5e5e5;
+`
