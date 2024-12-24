fcd2af26b3b7fe72a0fad18546151b72a47c2f45
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2024-12-24 10:48:08 -0300

    fix: 'next' and 'nanoid' vulnerabilities

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/bernardo/source/repos/bernas1104.github.io/src/app/layout.tsx
+// File: C:\Users\berna\source\repos\bernas1104.github.io\src\app\layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
