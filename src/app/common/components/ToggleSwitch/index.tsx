632ed38b0de12c1b31d8f8513467693f7614816b
bernas1104 <bernardoc1104@gmail.com>
2026-07-23 21:25:07 -0300

    chore: hard reset to Vite + React-TS

--- src/app/common/components/ToggleSwitch/index.tsx
+++
@@ -1,4 +0,0 @@
-import { ToggleSwitch } from './ToggleSwitch'
-import { IconToggleSwitch } from './IconToggleSwitch'
-
-export { ToggleSwitch, IconToggleSwitch }
--- src/app/common/components/ToggleSwitch/interfaces/IconToggleSwitchProps.ts
+++
@@ -1,6 +0,0 @@
-import ToggleSwitchProps from './ToggleSwitchProps'
-import { Icon } from '@/app/common/constants/icons'
-
-export default interface IconToggleSwitchProps extends ToggleSwitchProps {
-  icons: [Icon, Icon]
-}
