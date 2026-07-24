3c63fa863ee5a975d02a9985c795091b8bb7614a
bernas1104 <bernardoc1104@gmail.com>
2026-07-24 13:47:05 -0300

    chore: extended eslint for react; re-added test for pre-commit hook

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -15,10 +16,17 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat['jsx-runtime'],
       prettier,
     ],
     languageOptions: {
       globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: '19.2.8',
+      },
     },
   },
 ])
